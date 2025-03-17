9b39a0fddcb557e2cdac2ffbcb810c09c7ee2011
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-03-17 19:48:29 -0400

    Try fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,18 @@
-import mdx from '@next/mdx';
+import mdx from "@next/mdx";
 
 const withMDX = mdx();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = withMDX({
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'swiperjs.com',
-      },
+      { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "swiperjs.com" },
     ],
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
   },
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'], // Add MDX support
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"], // MDX support
+  output: "export", // Required for GitHub Pages
+  experimental: { mdxRs: false }, // Might help avoid conflicts with MDX for GitHub Pages
 });
 
 export default nextConfig;
