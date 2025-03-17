611b96bee187d5c2348c78ae90fcbf05eae62d9f
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-03-17 19:29:14 -0400

    Fixed

--- src/lib/apollo/apolloContextProvider.jsx
+++ src/lib/apollo/apolloContextProvider.jsx
@@ -1,16 +1,25 @@
 'use client';
 
-import { ApolloProvider } from '@apollo/client';
-import ApolloClient from './apolloClient';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 export function ApolloContextProvider({children}) {
 
-    return (
-        <>
-            <ApolloProvider client={ApolloClient}>
-                {children}
-            </ApolloProvider>
-        </>
+    // Check if environment variables are set
+    if (!process.env.NEXT_PUBLIC_SANITY_PROJECTID || !process.env.NEXT_PUBLIC_SANITY_DATASET) {
+        // Handle error condition here, such as logging an error or displaying a fallback UI
+        console.error('Environment variables NEXT_PUBLIC_SANITY_PROJECTID or NEXT_PUBLIC_SANITY_DATASET are not set.');
+        return <>Environment variables NEXT_PUBLIC_SANITY_PROJECTID or NEXT_PUBLIC_SANITY_DATASET are not set.</>; // Return null or a fallback component
+    }
 
-    )
+    // If environment variables are set, proceed with ApolloClient initialization
+    const sanityCDNUrl = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECTID}.api.sanity.io/v1/graphql/${process.env.NEXT_PUBLIC_SANITY_DATASET}/default`;
+
+    return (
+        <ApolloProvider client={new ApolloClient({
+            uri: sanityCDNUrl,
+            cache: new InMemoryCache(),
+        })}>
+        {children}
+        </ApolloProvider>
+      );
 }
