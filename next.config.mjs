63ac6c2a2a73e443ccf8c710e3aee13242acd31d
Dima <81911982+dimaTidev@users.noreply.github.com>
2025-03-17 21:19:19 -0400

    Removed mdx from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-import mdx from "@next/mdx";
-
-const withMDX = mdx();
-
-const nextConfig = withMDX({
+const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.youtube.com" },
@@ -10,9 +6,6 @@ const nextConfig = withMDX({
     ],
     domains: ["cdn.sanity.io"],
   },
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"], // MDX support
-  output: "export", // Required for GitHub Pages
-  experimental: { mdxRs: false }, // Might help avoid conflicts with MDX for GitHub Pages
-});
+};
 
 export default nextConfig;
